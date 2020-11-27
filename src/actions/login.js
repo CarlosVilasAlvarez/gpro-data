093d72a120a9e014b09bc630775fa302f9d8029c
CarlosVilasAlvarez <carlosvilasalvarez@gmail.com>
2020-11-27 19:10:54 +0100

    "Documented" login and screenshot

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,10 +1,12 @@
 async function logIn(page, username, password) {
   await page.goto("https://www.gpro.net/gb/gpro.asp");
+  // Click and fill username input box
   await page.click("#Text1");
   await page.keyboard.type(username);
+  // Click and fill password input box
   await page.click("#Password2");
   await page.keyboard.type(password);
-
+  // Submit login
   await Promise.all([page.waitForNavigation(), page.click("#Submit2")]);
 }
 
--- src/actions/screenshot.js
+++ src/actions/screenshot.js
@@ -1,13 +1,16 @@
 async function captureRaceAnalysis(page) {
+  //Enter to race analysis page
   await Promise.all([
     page.waitForNavigation(),
     page.click("#tlast > ul.xlastul > li.two.bg_smaller.br_smaller > a"),
   ]);
+  // Take screenshot
   await page.screenshot({
     path: "./race-analysis.jpg",
     quality: 80,
     clip: { x: 19, width: 944, y: 339, height: 1760 },
   });
+  // Return to home
   await Promise.all([
     page.waitForNavigation(),
     page.click("#nav > li.first > a"),
